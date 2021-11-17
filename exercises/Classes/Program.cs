f911cdcb92c12bd946445b74100b2ee650cd164c
JamaCodes <jama.avwork@gmail.com>
2021-11-17 15:31:29 -0600

    wishlist complete list must be called inside of method

--- exercises/Classes/Program.cs
+++ exercises/Classes/Program.cs
@@ -6,6 +6,11 @@ namespace Classes
     {
         static void Main(string[] args)
         {
+            Company myCompany = new Company
+            /*this is an object initializer vvv*/
+            {
+            Name = "Jamazon"
+            };
             // Create an instance of a company. Name it whatever you like.
 
             // Create three employees
@@ -16,6 +21,15 @@ namespace Classes
                 Iterate the company's employee list and generate the
                 simple report shown above
             */
+            
+
+            Employee jama = new Employee 
+            {
+                FirstName = "jama",
+                LastName = "Throwinthedamntowelmazon",
+                Title = "Goat",
+                StartDate = new DateTime(2021,11,17)
+            };
         }
     }
 }
